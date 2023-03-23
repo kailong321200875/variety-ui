e24835e6fde5a42c01006cda077f4d4cad70f9ec
kailong321200875 <321200875@qq.com>
2023-03-23 15:36:32 +0800

    init: init

---
+++ packages/core/src/components/timer/readme.md
@@ -0,0 +1,17 @@
+# view-timer
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property   | Attribute  | Description | Type     | Default |
+| ---------- | ---------- | ----------- | -------- | ------- |
+| `duration` | `duration` |             | `number` | `60`    |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
---
+++ packages/core/stencil.config.ts
@@ -0,0 +1,39 @@
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+import { postcss } from '@stencil-community/postcss';
+import autoprefixer from 'autoprefixer';
+
+export const config: Config = {
+  namespace: 'core',
+  plugins: [
+    sass({
+      // injectGlobalPaths: [
+      //   'src/globals/variables.scss',
+      //   'src/globals/mixins.scss'
+      // ]
+    }),
+    postcss({
+      plugins: [autoprefixer()],
+    }),
+  ],
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+    {
+      type: 'docs-vscode',
+      file: 'vscode-data.json',
+    },
+  ],
+};
